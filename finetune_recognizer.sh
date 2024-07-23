8f9c8d601458465941e8f7ae6fff44285962faf7
bkj <ubuntu@local>
2024-07-23 11:15:34 +0000

    small fixes

--- finetune_recognizer.sh
+++ finetune_recognizer.sh
@@ -1,19 +1,16 @@
 
 # BASE_DIR='/home/paperspace/zeek/'
-# mkdir -p $BASE_DIR + 'data/'
-# python generate_text.py --num_images 1000 --save_dir $BASE_DIR + 'data/leading_0s/'
+BASE_DIR='/home/paperspace/zeek/'
+
+# Create the directory
+mkdir -p "${BASE_DIR}data/"
+
+# Generate text
+# python generate_text.py --num_images 1000 --save_dir "${BASE_DIR}data/leading_0s/"
 
 # show improvement on leading 0s
-python train_recognizer.py \
-    --experiment_name 'leading_zeros_base' \
-    --data_dir $BASE_DIR + 'data/leading_0s/' \
-    --save_dir $BASE_DIR + 'models/leading_0s/' \
-    --n_train 500
+python train_recognizer.py --experiment_name 'leading_zeros_base' --data_path "${BASE_DIR}data/leading_0s/" --model_path "${BASE_DIR}models/leading_0s/" --n_train 500
 
 # show lack of degradation on normal data w leading 0s
-python train_recognizer.py \
-    --experiment_name 'leading_zeros_degrade' \
-    --data_dir $BASE_DIR + 'data/leading_0s/' \
-    --save_dir $BASE_DIR + 'models/leading_0s/' \
-    --n_train 500
+python train_recognizer.py --experiment_name 'leading_zeros_degrade' --data_path "${BASE_DIR}data/leading_0s/" --model_path "${BASE_DIR}models/leading_0s/" --n_train 500
 
--- train_recognizer.py
+++ train_recognizer.py
@@ -313,6 +313,7 @@ def parse_args():
 def main():
 
     args = parse_args()
+    print(args)
     os.makedirs(args.model_path, exist_ok=True)
     
     torch.cuda.empty_cache()
